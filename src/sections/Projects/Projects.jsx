ee3d5c2d661d3346128b7b28e6118898d73cca0b
Darasimi Omojola <omojoladarasimi9@gmail.com>
2025-03-31 16:38:49 +0100

    creating the skills section of this portfolio site

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,17 +1,38 @@
 import styles from "./ProjectsStyles.module.css";
 import viberr from "../../assets/viberr.png";
+import freshBurger from "../../assets/fresh-burger.png";
+import hipsster from "../../assets/hipsster.png";
+import fitlift from "../../assets/fitlift.png";
 import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
-      <h1 className="sectionTitle">Projects</h1>{" "}
+      <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
           src={viberr}
           link="https://github.com/Darasimi010"
           h3="Viberr"
           p="Streaming App"
+        />
+        <ProjectCard
+          src={freshBurger}
+          link="https://github.com/Darasimi010"
+          h3="Fresh Burger"
+          p="Hamburger Restaurant"
+        />
+        <ProjectCard
+          src={hipsster}
+          link="https://github.com/Darasimi010"
+          h3="Hipsster"
+          p="Glasses Shop"
+        />
+        <ProjectCard
+          src={fitlift}
+          link="https://github.com/Darasimi010"
+          h3="FitLift"
+          p="Fitness App"
         />
       </div>
     </section>
---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,36 @@
+import styles from "./SkillsStyles.module.css";
+import checkMarkIcon from "../../assets/checkmark-dark.svg";
+import SkillList from "../../common/SkillList";
+
+function Skills() {
+  return (
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="HTML" />
+        <SkillList src={checkMarkIcon} skill="CSS" />
+        <SkillList src={checkMarkIcon} skill="JavaScript" />
+        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkMarkIcon} skill="React" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Redux" />
+        <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="Bootstrap" />
+        <SkillList src={checkMarkIcon} skill="Github" />
+        <SkillList src={checkMarkIcon} skill="Express.js" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="MySQL" />
+        <SkillList src={checkMarkIcon} skill="MongoDB" />
+        <SkillList src={checkMarkIcon} skill="Responsive Web Design" />
+        <SkillList src={checkMarkIcon} skill="REST APIs" />
+        <SkillList src={checkMarkIcon} skill="npm" />
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
